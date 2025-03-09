474bab4f2a5c205c73c41435819d630fa5a25f4f
Invalid3000 <evgenoyoskinwww@gmail.com>
2025-03-10 00:06:35 +0700

    STEP-001

--- project/sprint_1/task_1/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ project/sprint_1/task_1/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -2,11 +2,13 @@ package org.javaguru.travel.insurance.core;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class TravelCalculatePremiumServiceImplTest {
 
     @Test
     public void deleteMe() {
-
+        assertTrue(true);
     }
 
 }
